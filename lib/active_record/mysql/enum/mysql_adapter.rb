4083272ee759435542c81be944dde49f8bd714a7
Yinon Rousso <yrousso@invoca.com>
2020-08-19 07:15:15 -0700

    no_jira refactor mysql adapter

--- lib/active_record/mysql/enum/mysql_adapter.rb
+++ lib/active_record/mysql/enum/mysql_adapter.rb
@@ -30,12 +30,16 @@ module ActiveRecord
         if Rails::VERSION::MAJOR < 5
           def type_to_sql(type, limit = nil, precision = nil, scale = nil, unsigned = nil, **_options) # :nodoc:
             if type.to_s == 'enum'
-              native = native_database_types[type]
-              column_type_sql = (native || {})[:name] || 'enum'
+              column_type =
+              if (native_database_type = native_database_types[type])
+                native_database_type[:name]
+              else
+                'enum'
+              end
 
-              column_type_sql += "(#{limit.map { |v| quote(v) }.join(',')})"
+              quoted_values = limit.map { |v| quote(v) }.join(',')
 
-              column_type_sql
+              "#{column_type}(#{quoted_values})"
             else
               super(type, limit, precision, scale, unsigned)
             end
@@ -43,12 +47,16 @@ module ActiveRecord
         else
           def type_to_sql(type, limit: nil, precision: nil, scale: nil, unsigned: nil, **_options) # :nodoc:
             if type.to_s == 'enum'
-              native = native_database_types[type]
-              column_type_sql = (native || {})[:name] || 'enum'
+              column_type =
+              if (native_database_type = native_database_types[type])
+                native_database_type[:name]
+              else
+                'enum'
+              end
 
-              column_type_sql += "(#{limit.map { |v| quote(v) }.join(',')})"
+              quoted_values = limit.map { |v| quote(v) }.join(',')
 
-              column_type_sql
+              "#{column_type}(#{quoted_values})"
             else
               super(type, limit: limit, precision: precision, scale: scale, unsigned: unsigned)
             end
