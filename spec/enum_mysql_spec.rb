93838bd559223058fd1b7039ae9dac3410f5107c
Drew Caddell <dcaddell@invoca.com>
2020-09-01 20:42:16 -0700

    TECH-4739: Finish converting the enum_mysql_spec to rspec

--- spec/enum_mysql_spec.rb
+++ spec/enum_mysql_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'fixtures/enumeration_test_model'
+# require 'fixtures/enum_test_controller'
 
 describe EnumerationTestModel do
   before :each do
@@ -44,166 +45,188 @@ describe EnumerationTestModel do
     expect(row.errors['color']).to eq(['is not included in the list'])
   end
 
-#   def test_other_types
-#     row = EnumerationTestModel.new
-#     row.string_field = 'a' * 10
-#     assert !row.save
-#     assert_equal ['is too long (maximum is 8 characters)'], row.errors['string_field']
-#
-#     row = EnumerationTestModel.new
-#     assert !row.save
-#     assert_equal ['can\'t be blank'], row.errors['string_field']
-#
-#     row = EnumerationTestModel.new
-#     row.string_field = 'test'
-#     row.int_field = 'aaaa'
-#     assert !row.save
-#     assert_equal ['is not a number'], row.errors['int_field']
-#
-#     row = EnumerationTestModel.new
-#     row.string_field = 'test'
-#     row.int_field = '500'
-#     assert row.save
-#   end
-#
-#   def test_view_helper
-#     request  = ActionController::TestRequest.new
-#     response = ActionController::TestResponse.new
-#     request.action = 'test1'
-#     body = EnumController.process(request, response).body
-#     assert_equal '<select id="test_severity" name="test[severity]"><option value="low">low</option><option value="medium" selected="selected">medium</option><option value="high">high</option><option value="critical">critical</option></select>', body
-#   end
-#
-#   def test_radio_helper
-#     request  = ActionController::TestRequest.new
-#     response = ActionController::TestResponse.new
-#     request.action = 'test2'
-#     body = EnumController.process(request, response).body
-#     assert_equal '<label>low: <input id="test_severity_low" name="test[severity]" type="radio" value="low" /></label><label>medium: <input checked="checked" id="test_severity_medium" name="test[severity]" type="radio" value="medium" /></label><label>high: <input id="test_severity_high" name="test[severity]" type="radio" value="high" /></label><label>critical: <input id="test_severity_critical" name="test[severity]" type="radio" value="critical" /></label>', body
-#   end
-#
-#
-#   # Basic tests
-#   def test_create_basic_default
-#     assert (object = BasicEnumTestModel.create)
-#     assert_nil object.value,
-#       "Enum columns without explicit default, default to null if allowed"
-#     assert !object.new_record?
-#   end
-#
-#   def test_create_basic_good
-#     assert (object = BasicEnumTestModel.create(:value => :good))
-#     assert_equal :good, object.value
-#     assert !object.new_record?
-#     assert (object = BasicEnumTestModel.create(:value => :working))
-#     assert_equal :working, object.value
-#     assert !object.new_record?
-#   end
-#
-#   def test_create_basic_null
-#     assert (object = BasicEnumTestModel.create(:value => nil))
-#     assert_nil object.value
-#     assert !object.new_record?
-#   end
-#
-#   def test_create_basic_bad
-#     assert (object = BasicEnumTestModel.create(:value => :bad))
-#     assert object.new_record?
-#   end
-#
-#   # Basic w/ Default
-#
-#   ######################################################################
-#
-#   def test_create_basic_wd_default
-#     assert (object = BasicDefaultEnumTestModel.create)
-#     assert_equal :working, object.value, "Explicit default ignored columns"
-#     assert !object.new_record?
-#   end
-#
-#   def test_create_basic_wd_good
-#     assert (object = BasicDefaultEnumTestModel.create(:value => :good))
-#     assert_equal :good, object.value
-#     assert !object.new_record?
-#     assert (object = BasicDefaultEnumTestModel.create(:value => :working))
-#     assert_equal :working, object.value
-#     assert !object.new_record?
-#   end
-#
-#   def test_create_basic_wd_null
-#     assert (object = BasicDefaultEnumTestModel.create(:value => nil))
-#     assert_nil object.value
-#     assert !object.new_record?
-#   end
-#
-#   def test_create_basic_wd_bad
-#     assert (object = BasicDefaultEnumTestModel.create(:value => :bad))
-#     assert object.new_record?
-#   end
-#
-#
-#
-#   # Nonnull
-#
-#   ######################################################################
-#
-#   def test_create_nonnull_default
-#     assert (object = NonnullEnumTestModel.create)
-# #    assert_equal :good, object.value,
-# #      "Enum columns without explicit default, default to first value if null not allowed"
-#     assert object.new_record?
-#   end
-#
-#   def test_create_nonnull_good
-#     assert (object = NonnullEnumTestModel.create(:value => :good))
-#     assert_equal :good, object.value
-#     assert !object.new_record?
-#     assert (object = NonnullEnumTestModel.create(:value => :working))
-#     assert_equal :working, object.value
-#     assert !object.new_record?
-#   end
-#
-#   def test_create_nonnull_null
-#     assert (object = NonnullEnumTestModel.create(:value => nil))
-#     assert object.new_record?
-#   end
-#
-#   def test_create_nonnull_bad
-#     assert (object = NonnullEnumTestModel.create(:value => :bad))
-#     assert object.new_record?
-#   end
-#
-#   # Nonnull w/ Default
-#
-#   ######################################################################
-#
-#   def test_create_nonnull_wd_default
-#     assert (object = NonnullDefaultEnumTestModel.create)
-#     assert_equal :working, object.value
-#     assert !object.new_record?
-#   end
-#
-#   def test_create_nonnull_wd_good
-#     assert (object = NonnullDefaultEnumTestModel.create(:value => :good))
-#     assert_equal :good, object.value
-#     assert !object.new_record?
-#     assert (object = NonnullDefaultEnumTestModel.create(:value => :working))
-#     assert_equal :working, object.value
-#     assert !object.new_record?
-#   end
-#
-#   def test_create_nonnull_wd_null
-#     assert (object = NonnullDefaultEnumTestModel.create(:value => nil))
-#     assert object.new_record?
-#   end
-#
-#   def test_create_nonnull_wd_bad
-#     assert (object = NonnullDefaultEnumTestModel.create(:value => :bad))
-#     assert object.new_record?
-#   end
-#
-#   def test_quoting
-#     value = EnumerationTestModel.send(:sanitize_sql, ["value = ? ", :"'" ] )
-#     assert_equal "value = '\\'' ", value
-#   end
+  it 'test_other_types' do
+    row = EnumerationTestModel.new
+    row.string_field = 'a' * 10
+    expect(row.save).to be false
+    expect(row.errors['string_field']).to eq(['is too long (maximum is 8 characters)'])
+
+    row = EnumerationTestModel.new
+    expect(row.save).to be false
+    expect(row.errors['string_field']).to eq(['can\'t be blank'])
+
+    row = EnumerationTestModel.new
+    row.string_field = 'test'
+    row.int_field = 'aaaa'
+    expect(row.save).to be false
+    expect(row.errors['int_field']).to eq(['is not a number'])
+
+    row = EnumerationTestModel.new
+    row.string_field = 'test'
+    row.int_field = '500'
+    expect(row.save).to be true
+  end
+
+  # it 'test_view_helper' do
+  #   request  = ActionController::TestRequest.new
+  #   response = ActionController::TestResponse.new
+  #   request.action = 'test1'
+  #   body = EnumTestController.process(request, response).body
+  #   expect(body).to eq('<select id="test_severity" name="test[severity]"><option value="low">low</option><option value="medium" selected="selected">medium</option><option value="high">high</option><option value="critical">critical</option></select>')
+  # end
+  #
+  # it 'test_radio_helper' do
+  #   request  = ActionController::TestRequest.new
+  #   response = ActionController::TestResponse.new
+  #   request.action = 'test2'
+  #   body = EnumTestController.process(request, response).body
+  #   expect(body).to eq('<label>low: <input id="test_severity_low" name="test[severity]" type="radio" value="low" /></label><label>medium: <input checked="checked" id="test_severity_medium" name="test[severity]" type="radio" value="medium" /></label><label>high: <input id="test_severity_high" name="test[severity]" type="radio" value="high" /></label><label>critical: <input id="test_severity_critical" name="test[severity]" type="radio" value="critical" /></label>')
+  # end
+
+
+  # Basic tests
+  it 'test_create_basic_default' do
+    object = BasicEnumTestModel.create
+    expect(object).to be_truthy
+    expect(object.value).to be_nil
+    expect(object.new_record?).to be false
+  end
+
+  it 'test_create_basic_good' do
+    object = BasicEnumTestModel.create(:value => :good)
+    expect(object).to be_truthy
+    expect(object.value).to eq(:good)
+    expect(object.new_record?).to be false
+    
+    object = BasicEnumTestModel.create(:value => :working)
+    expect(object).to be_truthy
+    expect(object.value).to eq(:working)
+    expect(object.new_record?).to be false
+  end
+
+  it 'test_create_basic_null' do
+    object = BasicEnumTestModel.create(:value => nil)
+    expect(object).to be_truthy
+    expect(object.value).to be_nil
+    expect(object.new_record?).to be false
+  end
+
+  it 'test_create_basic_bad' do
+    object = BasicEnumTestModel.create(:value => :bad)
+    expect(object).to be_truthy
+    expect(object.new_record?).to be true
+  end
+
+  # Basic w/ Default
+
+  ######################################################################
+
+  it 'test_create_basic_wd_default' do
+    object = BasicDefaultEnumTestModel.create
+		expect(object).to be_truthy
+    expect(object.value).to eq(:working)
+    expect(object.new_record?).to be false
+  end
+
+  it 'test_create_basic_wd_good' do
+    object = BasicDefaultEnumTestModel.create(:value => :good)
+		expect(object).to be_truthy
+    expect(object.value).to eq(:good)
+    expect(object.new_record?).to be false
+    
+    object = BasicDefaultEnumTestModel.create(:value => :working)
+		expect(object).to be_truthy
+    expect(object.value).to eq(:working)
+    expect(object.new_record?).to be false
+  end
+
+  it 'test_create_basic_wd_null' do
+    object = BasicDefaultEnumTestModel.create(:value => nil)
+		expect(object).to be_truthy
+    expect(object.value).to be nil
+    expect(object.new_record?).to be false
+  end
+
+  it 'test_create_basic_wd_bad' do
+    object = BasicDefaultEnumTestModel.create(:value => :bad)
+		expect(object).to be_truthy
+    expect(object.new_record?).to be true
+  end
+
+
+
+  # Nonnull
+
+  ######################################################################
+
+  it 'test_create_nonnull_default' do
+    object = NonnullEnumTestModel.create
+		expect(object).to be_truthy
+    expect(object.value).to be nil # Enum columns without explicit value don't default to first value if null not allowed
+    expect(object.new_record?).to be true
+  end
+
+  it 'test_create_nonnull_good' do
+    object = NonnullEnumTestModel.create(:value => :good)
+		expect(object).to be_truthy
+    expect(object.value).to eq(:good)
+    expect(object.new_record?).to be false
+
+    object = NonnullEnumTestModel.create(:value => :working)
+		expect(object).to be_truthy
+    expect(object.value).to eq(:working)
+    expect(object.new_record?).to be false
+  end
+
+  it 'test_create_nonnull_null' do
+    object = NonnullEnumTestModel.create(:value => nil)
+		expect(object).to be_truthy
+    expect(object.new_record?).to be true
+  end
+
+  it 'test_create_nonnull_bad' do
+    object = NonnullEnumTestModel.create(:value => :bad)
+		expect(object).to be_truthy
+    expect(object.new_record?).to be true
+  end
+
+  # Nonnull w/ Default
+
+  ######################################################################
+
+  it 'test_create_nonnull_wd_default' do
+    object = NonnullDefaultEnumTestModel.create
+		expect(object).to be_truthy
+    expect(object.value).to eq(:working)
+    expect(object.new_record?).to be false
+  end
+
+  it 'test_create_nonnull_wd_good' do
+    object = NonnullDefaultEnumTestModel.create(:value => :good)
+		expect(object).to be_truthy
+    expect(object.value).to eq(:good)
+    expect(object.new_record?).to be false
+
+    object = NonnullDefaultEnumTestModel.create(:value => :working)
+		expect(object).to be_truthy
+    expect(object.value).to eq(:working)
+    expect(object.new_record?).to be false
+  end
+
+  it 'test_create_nonnull_wd_null' do
+    object = NonnullDefaultEnumTestModel.create(:value => nil)
+		expect(object).to be_truthy
+    expect(object.new_record?).to be true
+  end
+
+  it 'test_create_nonnull_wd_bad' do
+    object = NonnullDefaultEnumTestModel.create(:value => :bad)
+		expect(object).to be_truthy
+    expect(object.new_record?).to be true
+  end
+
+  it 'test_quoting' do
+    value = EnumerationTestModel.send(:sanitize_sql, ["value = ? ", :"'" ] )
+    expect(value).to eq("value = '\\'' ")
+  end
 end
--- spec/fixtures/enum_test_controller.rb
+++ spec/fixtures/enum_test_controller.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
-class EnumController < ActionController::Base
+class EnumTestController < ActionController::Base
   layout false
 
   def enum_select
     @test = EnumerationTestModel.new
     render :inline => "<%= enum_select('test', 'severity')%>"
   end
-
 end
