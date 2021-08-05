71f3431e801a31854725187fa13c3ebfadf7f37e
Iris Moini-Nazeri <imoini-nazeri@invoca.com>
2021-08-05 11:31:05 -0700

    TECH-6150 dropped support for rails 4

--- spec/unit/enum_column_adapter_spec.rb
+++ spec/unit/enum_column_adapter_spec.rb
@@ -33,31 +33,6 @@ describe ActiveRecord::Mysql::Enum::EnumColumnAdapter do
     it "creates a new Column" do
       expect(test_column).to be_a ActiveRecord::Mysql::Enum::ActiveRecordColumnWithEnums
     end
-
-    if Rails::VERSION::MAJOR < 5
-      context "#type_cast_from_database" do
-        it "converts strings to symbols" do
-          expect(test_column.type_cast_from_database("pizza")).to eq(:pizza)
-        end
-
-        it "leaves symbols as symbols" do
-          expect(test_column.type_cast_from_database(:pizza)).to eq(:pizza)
-        end
-
-        it "returns nil for other data types" do
-          [
-            1,
-            1.5,
-            Time.now,
-            true,
-            ["fun"],
-            { pizza: 'party' }
-          ].each do |value|
-            expect(test_column.type_cast_from_database(value)).to be nil
-          end
-        end
-      end
-    end
   end
 
   context "as a String column (calling super)" do
@@ -65,18 +40,6 @@ describe ActiveRecord::Mysql::Enum::EnumColumnAdapter do
 
     it "creates a new Column" do
       expect(test_column).to be_a ActiveRecord::Mysql::Enum::ActiveRecordColumnWithEnums
-    end
-
-    if Rails::VERSION::MAJOR < 5
-      context "#type_cast_from_database" do
-        it "leaves strings as strings" do
-          expect(test_column.type_cast_from_database("pizza")).to eq("pizza")
-        end
-
-        it "converts symbols to strings" do
-          expect(test_column.type_cast_from_database(:pizza)).to eq("pizza")
-        end
-      end
     end
   end
 end
